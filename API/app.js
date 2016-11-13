c3698fe2128746b82f0326d3103b72579b2b80af
Joseph <jcdaniel55@gmail.com>
2016-11-13 18:18:56 -0500

    update DAL & API; create addViews.js

--- API/app.js
+++ API/app.js
@@ -10,6 +10,24 @@ app.use(bodyParser.json())
 const server = http.createServer(app)
 server.listen(port, () => console.log('server opened on ', server.address(), 'port: ', port))
 
+
+//////////////////////////////////
+///////GET all snowboards/////////
+//////////////////////////////////
+
+app.get('/snowboards', function(req, res, next) {
+  dal.listAllBoards(req.body, function(data, err) {
+    if (err) {
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
+  }
+    if (data) {
+      res.append('Content-type', 'application/json')
+      res.status(200).send(data)
+    }
+  })
+});
+
 //////////////////////////////////
 ///////GET snowboard by id////////
 //////////////////////////////////
@@ -19,7 +37,8 @@ app.get('/snowboards/:id', function(req, res, next) {
   console.log("id:", boardId) //works
   dal.getBoardById(boardId, function(data, err) {
     if (err) {
-    return console.log(err.message)
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
   }
     if (data) {
       res.append('Content-type', 'application/json')
@@ -35,7 +54,8 @@ app.get('/snowboards/:id', function(req, res, next) {
 app.post('/snowboards', function(req, res, next) {
   dal.createEntry(req.body, function(err, data) {
     if (err) {
-      return console.log(err.message)
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
     }
     if (data) {
       res.append('Content-type', 'application/json')
@@ -51,7 +71,8 @@ app.post('/snowboards', function(req, res, next) {
 app.put('/snowboards/:id', function(req, res, next) {
   dal.editEntry(req.body, function(err, data) {
     if (err) {
-      return console.log(err.message)
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
     }
     if (data) {
       res.append('Content-type', 'application/json')
@@ -60,15 +81,42 @@ app.put('/snowboards/:id', function(req, res, next) {
   })
 });
 
+////////// DELETE Entry //////////
+
+// app.delete('/snoboards/:id', function(req, res, next) {
+//   console.log('app.delete')
+//   const BoardId = req.params.id
+//   dal.getBoardById(BoardId, function(err, data) {
+//     console.log('dal.getBoardById')
+//     if (err) {
+//       var responseError = BuildResponseError(err);
+//         return next(new HTTPError(responseError.status, responseError.message, responseError));
+//     }
+//     if (data) {
+//       dal.deleteEntry(data, function(deleteErr, deleteData) {
+//         if (deleteErr) {
+//           var responseError = BuildResponseError(deleteErr);
+//           return next(new HTTPError(responseError.status, responseError.message, responseError));
+//         }
+//         if (deleteData) {
+//           res.append('Content-type', 'application/json')
+//           res.status(202).send(deleteData)
+//         }
+//       })
+//     }
+//   })
+// });
+
 //////////////////////////////////
 //////list boards by type/////////
 //////////////////////////////////
 
-app.get('/snowboards_by_type', function(req, res, next) {
-  const type = req.query.type
+app.get('/snowboard_type', function(req, res, next) {
+  const type = req.query.Type
   dal.listByType(type, function(err, data) {
     if (err) {
-      return err.message
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
     }
     if (data) {
       res.append('Content-type', 'application/json')
@@ -77,40 +125,91 @@ app.get('/snowboards_by_type', function(req, res, next) {
   })
 });
 
+//////////////////////////////////
+////// list boards by Make ///////
+//////////////////////////////////
+
+app.get('/snowboard_make', function(req, res, next) {
+  const make = req.query.Make
+  dal.listByMake(make, function(err, data) {
+    if (err) {
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
+    }
+    if (data) {
+      res.append('Content-type', 'application/json')
+      res.status(200).send(data)
+    }
+  })
+});
+
+//////////////////////////////////
+///// list boards by Length //////
+//////////////////////////////////
+
+app.get('/snowboard_length', function(req, res, next) {
+  const length = req.query.Length
+  dal.listByLength(length, function(err, data) {
+    //console.log(length)
+    if (err) {
+      var responseError = BuildResponseError(err);
+      return next(new HTTPError(responseError.status, responseError.message, responseError));
+    }
+    if (data) {
+      res.append('Content-type', 'application/json')
+      res.status(200).send(data)
+    }
+  })
+});
+
+////////////////////////////
+//// BuildResponseError ////
+////////////////////////////
+
+function BuildResponseError(err) {
+
+  const statuscheck = isNaN(err.message.substring(0, 3)) === true ? "400" : err.message.substring(0, 3)
+  const status = err.status ? Number(err.status) : Number(statuscheck)
+  const message = err.status ? err.message : err.message.substring(3)
+  const reason = "messagenan"
+  const error = status === 400 ? "Bad Request" : err.name
+  const name = error
+
+  var errormsg = {}
+  errormsg.error = error
+  errormsg.reason = reason
+  errormsg.name = name
+  errormsg.status = status
+  errormsg.message = message
+  return errormsg
+};
+
 /////////////////////////////
 ////// Error Handling ///////
 /////////////////////////////
 
-app.get('*', function(req, res, next) {
-  next(new HTTPError(404, 'Path not found', {
-    placeholderObject: "future errors"
-  }))
-  return next(routeError)
-})
-
-app.put('*', function(req, res, next) {
-  next(new HTTPError(404, 'Path not found', {
-    placeholderObject: "future errors"
-  }))
-  return next(routeError)
-})
-
-app.post('*', function(req, res, next) {
-  next(new HTTPError(404, 'Path not found', {
-    placeholderObject: "future errors"
-  }))
-  return next(routeError)
-})
-
-app.delete('*', function(req, res, next) {
-  next(new HTTPError(404, 'Path not found', {
-    placeholderObject: "future errors"
-  }))
-  return next(routeError)
-})
-
-app.use(function(err, req, res, next) {
-  console.log(req.method, " ", req.path, " ", err)
-  res.status(err.status || 500)
-  res.send('path not valid')
-})
+// app.get('*', function(req, res, next) {
+//   console.log('in get *')
+//   next(new HTTPError(404, 'Invalid path'));
+// })
+//
+// app.put('*', function(req, res, next) {
+//   console.log('in put *')
+//   next(new HTTPError(404, 'Invalid path'));
+// })
+//
+// app.post('*', function(req, res, next) {
+//   console.log('in post *')
+//   next(new HTTPError(404, 'Invalid path'));
+// })
+//
+// app.delete('*', function(req, res, next) {
+//   console.log('in delete *')
+//   next(new HTTPError(404, 'Invalid path'));
+// })
+//
+// app.use(function(err, req, res, next) {
+//   console.log(req.method, " ", req.path, " ", err)
+//   res.status(err.status || 500)
+//   res.send(res.method, " ", res.path, " ", err)
+// })
--- DAL/dalNoSQL.js
+++ DAL/dalNoSQL.js
@@ -1,31 +1,54 @@
 const PouchDB = require('pouchdb-http');
 PouchDB.plugin(require('pouchdb-mapreduce'));
 
-
-const db = new PouchDB('http://localhost:5984/api-exam')
+const db = new PouchDB('http://localhost:5984/api-exam2')
 
 const dal = {
   createEntry: createEntry,
   editEntry: editEntry,
-  // deleteEntry: deleteEntry,
+  //deleteEntry: deleteEntry,
   getBoardById: getBoardById,
-  // listByMake: listByMake,
-  // listBySize: listBySize,
+  listAllBoards: listAllBoards,
+  listByMake: listByMake,
+  listByLength: listByLength,
   // listByMSRP: listByMSRP,
   // listByFlex: listByFlex,
-  listByType: listByType
+  listByType: listByType,
+  createView: createView,
 }
+
+/////////////////////////////////////
+///////// Wrapper Functions /////////
+/////////////////////////////////////
 
 function getBoardById(boardId, callback) {
   getDocById(boardId, callback)
 }
+
 function listByType(type, callback) {
   queryBoardByType(type, callback)
 }
 
+function listByType(type, callback) {
+  queryBoardByType(type, callback)
+}
+
+function listByMake(make, callback) {
+  queryBoardByMake(make, callback)
+}
+
+function listByLength(length, callback) {
+  queryBoardByLength(length, callback)
+}
+
+// function deleteEntry(data, callback) {
+//   deleteDoc(data, callback)
+// }
 /////////////////////////////////////
 ////////// Base Functions ///////////
 /////////////////////////////////////
+
+////////// Create Entry /////////////
 
 function createEntry(data, callback) {
   if (data.hasOwnProperty('make') !== true) {
@@ -53,6 +76,8 @@ function createEntry(data, callback) {
   })
 };
 
+///////////// Edit Entry ////////////////
+
 function editEntry(data, callback) {
   if (data.hasOwnProperty('_id') !== true) {
     return callback(new Error('400 Missing _id property'))
@@ -67,6 +92,50 @@ function editEntry(data, callback) {
     console.log('202 Edits Accepted')
   })
 };
+
+/////////////////////////////////////
+///////// Delete Document ///////////
+/////////////////////////////////////
+
+// function deleteDoc(data, callback) {
+//   if (data === undefined || data === null) {
+//     return callback(new Error('400Missing data'))
+//   }
+//   if (data.hasOwnProperty('_id') !== true) {
+//     return callback(new Error('400Missing _id property within data'))
+//   }
+//   if (data.hasOwnProperty('_rev') !== true) {
+//     return callback(new Error('400Missing _rev property within data'))
+//   }
+//   // db.get(data, function(err, data){
+//   //   if (err) {
+//   //     return console.log(err)
+//   //   }
+//   db.remove(data, function(err, result) {
+//       if (err) return callback(err)
+//       if (result) return callback(null, result)
+//     })
+//     // })
+// }
+//////////// List All Snowboards /////////////
+
+function listAllBoards(data, callback) {
+  db.allDocs({
+    include_docs: true,
+    startkey: 'snowboard_',
+    endkey: 'snowboard_\uffff',
+    attachments: true
+  }, function(err, response) {
+    if (err) {
+      return console.log(err);
+    }
+    if (response) {
+      return callback(response)
+    }
+  })
+};
+
+/////////// Get Snowboard by ID ////////////
 
 function getDocById(boardId, callback) {
   if (typeof boardId === "undefined" || boardId === null) {
@@ -89,12 +158,12 @@ function getDocById(boardId, callback) {
 /////////////////////////////////////
 
 const getType = function(queryRow) {
-  queryRow.value.type = queryRow.key
+  queryRow.value.Type = queryRow.key
   return queryRow.value
 };
 
 function queryBoardByType(type, callback) {
-console.log("fnQueryBoardByType:", type)
+//console.log("fnQueryBoardByType:", type)
   db.query('boardByType', {
     key: type
   }, function(err, result) {
@@ -106,11 +175,58 @@ console.log("fnQueryBoardByType:", type)
       return callback(null, result.rows.map(getType));
     }
   })
-}
-
+};
 
 /////////////////////////////////////
-////////////// Views ////////////////
+///////Query Board by Make///////////
+/////////////////////////////////////
+
+const getMake = function(queryRow) {
+  queryRow.value.Make = queryRow.key
+  return queryRow.value
+};
+
+function queryBoardByMake(make, callback) {
+console.log("fnQueryBoardByMake:", make)
+  db.query('boardByMake', {
+    key: make
+  }, function(err, result) {
+    if (err) {
+      return callback(err)
+    }
+    if (result) {
+      console.log("result:", result, "end")
+      return callback(null, result.rows.map(getMake));
+    }
+  })
+};
+
+/////////////////////////////////////
+/////// Query Board by Length ///////
+/////////////////////////////////////
+
+const getLength = function(queryRow) {
+  queryRow.value.Length = queryRow.key
+  return queryRow.value
+};
+
+function queryBoardByLength(length, callback) {
+  console.log(length)
+  db.query('boardByLength', {
+    key: length
+  }, function(err, result) {
+    if (err) {
+      return callback(err)
+    }
+    if (result) {
+      console.log("result:", result, "end")
+      return callback(null, result.rows.map(getLength));
+    }
+  })
+};
+
+/////////////////////////////////////
+///////////Create View //////////////
 /////////////////////////////////////
 
 function createView(view) {
@@ -122,24 +238,6 @@ function createView(view) {
       console.log("response: ", JSON.stringify(response, null, 4))
     }
   })
-};
-
-const boardByType = {
-  _id: "_design/boardByType",
-  views: {
-    boardByType: {
-      map: function(doc) {
-        if (doc.type === "snowboard") {
-          emit(doc.description, {
-            "make": doc.make,
-            "model": doc.model,
-            "length": doc.length,
-            "flex": doc.flex
-          });
-        }
-      }.toString()
-    }
-  }
 };
 
 module.exports = dal
