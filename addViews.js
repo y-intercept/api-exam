ae056b8e557541604060cbd919f8236d6b35aebb
Joseph Daniel <jcdaniel55@gmail.com>
2016-11-13 18:31:21 -0500

    create addViews.js to create design docs

---
+++ addViews.js
@@ -0,0 +1,84 @@
+const dal = require('./DAL/dalNoSQL.js');
+
+/////////////////////////////////////
+///////////// Views /////////////////
+/////////////////////////////////////
+
+const boardByType = {
+  _id: "_design/boardByType",
+  views: {
+    boardByType: {
+      map: function(doc) {
+        if (doc.description === "snowboard") {
+          emit(doc.type, {
+            "make": doc.make,
+            "model": doc.model,
+            "length": doc.length + 'cm',
+            "flex": doc.flex
+          });
+        }
+      }.toString()
+    }
+  }
+};
+
+const boardByMake = {
+  _id: "_design/boardByMake",
+  views: {
+    boardByMake: {
+      map: function(doc) {
+        if (doc.description === "snowboard") {
+          emit(doc.make, {
+            "make": doc.make,
+            "model": doc.model,
+            "length": doc.length + 'cm',
+            "flex": doc.flex
+          });
+        }
+      }.toString()
+    }
+  }
+};
+
+const boardByLength = {
+  _id: "_design/boardByLength",
+  views: {
+    boardByLength: {
+      map: function(doc) {
+        if (doc.description === "snowboard") {
+          emit(doc.length, {
+            "make": doc.make,
+            "model": doc.model,
+            "length": doc.length + 'cm',
+            "flex": doc.flex
+          });
+        }
+      }.toString()
+    }
+  }
+};
+
+dal.createView(boardByType)
+dal.createView(boardByMake)
+dal.createView(boardByLength)
+
+// dal.createView(boardByType, function callback(err, data) {
+//     if (err) return console.log(err);
+//     if (data) {
+//         console.log(data);
+//     }
+// })
+//
+// dal.createView(boardByMake, function callback(err, data) {
+//     if (err) return console.log(err);
+//     if (data) {
+//         console.log(data);
+//     }
+// })
+//
+// dal.createView(boardByLength, function callback(err, data) {
+//     if (err) return console.log(err);
+//     if (data) {
+//         console.log(data);
+//     }
+// })
